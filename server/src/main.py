7d82b54c4324cce55c3a13f9ae555e69088dd27a
Gagan R Shetty <gaganrshetty07@gmail.com>
2026-08-30 11:36:52 +0530

    imp:  implement client-side session error handling

--- server/src/main.py
+++ server/src/main.py
@@ -27,7 +27,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://0.0.0.0:5500", "http://127.0.0.1:5500"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
